9085ee32ce7fae15fedbdba465e70a39ca9e5201
Banana Chiu <agimz0330@gmail.com>
2024-11-15 18:28:19 +0800

    Initial Commit

---
+++ Gametopia/GametopiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GametopiaApp.swift
+//  Gametopia
+//
+//  Created by 邱詩 on 2024/11/15.
+//
+
+import SwiftUI
+
+@main
+struct GametopiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
